213c6f9ead1913a51b407cc5174b5cf9439515c5
harchockk <urao54840@gmail.com>
2025-11-06 18:51:11 +0300

    htsrdu

--- script/script.js
+++ script/script.js
@@ -6,3 +6,91 @@
          behavior:'smooth'
      });
  });
+
+
+ console.log('Привет мир');
+
+
+let massage;
+massage = 'Hello';
+
+
+
+for (let index = 1; index < 2; index++) {
+
+    alert(massage);
+}
+
+
+
+let favoriteFood = "пицца";
+
+for (let index = 1; index < 7; index++) {
+    
+    console.log(favoriteFood);
+}
+
+console.log(favoriteFood); 
+
+favoriteFood="cушка";
+
+console.log(favoriteFood);
+
+let age = 145;
+age = 31;
+console.log(age);
+
+const pi = 3.1;
+
+let $$$$$ = 1;
+let ____________________________________________________________________________________________________________$$$$$$$$$$$$$$$$$$$$$_______________________bn = 4;
+
+alert($$$$$ + ____________________________________________________________________________________________________________$$$$$$$$$$$$$$$$$$$$$_______________________bn)
+
+
+let 阴道_屁股_阴茎 = 6;
+
+
+let nig = 123;
+nig = 12.345;
+
+
+let isSun = true;
+
+let isNoSun = false;
+
+
+if(isSun)
+{
+    console.log("РНДГЕГКН")
+}
+
+
+let box = null;
+let box2;
+console.log(box,box2);
+
+
+let item = {
+    title:"Книга",
+    pages:200,
+    available:true
+};
+
+console.log(item.title);
+
+item.title = "Нига";
+
+console.log(item.title);
+
+
+let colors = ["red","nered","neredagreen"];
+
+for (let index = 0; index < colors.length; index++) {
+    console.log(typeof colors[index],": ",colors[index]);
+    
+}
+
+console.log(`Мой любимый цвет - ${colors[0]}!`)
+
+
